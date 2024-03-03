e50c89aedd4b41da4b5b7d3dc1954f85d3f9bd6e
Kolodich <k0l0dic4rus@gmail.com>
2024-03-03 16:34:31 +0300

    feat(game.html): Добавялет спойлеры для таблиц и списков

--- src/js/utils.js
+++ src/js/utils.js
@@ -68,3 +68,71 @@ export function debugAfter(delay) {
 		debugger
 	}, delay)
 }
+
+/**
+ * @template T
+ * @param {T|NodeList} value
+ * @returns {T[]}
+ */
+export function castToArray(value) {
+	if (value instanceof NodeList) {
+		return [...value];
+	}
+	return Array.isArray(value) ? value : [value]
+}
+
+/**
+ * @param {string|HTMLElement} elOrSelector
+ * @param {HTMLElement} target
+ * @returns {HTMLElement}
+ */
+export function getEl(elOrSelector, target = document) {
+	if (typeof elOrSelector === 'string') {
+		return target.querySelector(elOrSelector);
+	}
+	return elOrSelector;
+}
+
+/**
+ * @param {string|HTMLElement[]|NodeList} elsOrSelector
+ * @param {HTMLElement} target
+ * @returns {HTMLElement[]}
+ */
+export function getEls(elsOrSelector, target = document) {
+	if (typeof elsOrSelector === 'string') {
+		return [...target.querySelectorAll(elsOrSelector)];
+	}
+	return castToArray(elsOrSelector);
+}
+
+/**
+ * @param {string|HTMLElement} elOrSelector
+ * @param {boolean} bool
+ */
+export function setExpanded(elOrSelector, bool) {
+	getEl(elOrSelector).setAttribute('aria-expanded', bool ? 'true' : 'false')
+}
+
+/**
+ * @param {string|HTMLElement|HTMLElement[]|NodeList} elsOrSelector 
+ * @param {string|string[]} _class 
+ * @param {HTMLElement} target 
+ */
+export function addClass(elsOrSelector, _class, target = document) {
+	const _els = getEls(elsOrSelector, target);
+	_els.forEach(el => {
+		el.classList.add(...castToArray(_class));
+	});
+}
+
+/**
+ * @param {string|HTMLElement|HTMLElement[]|NodeList} elsOrSelector 
+ * @param {string|string[]} _class 
+ * @param {HTMLElement} target 
+ */
+export function removeClass(elsOrSelector, _class, target = document) {
+	const _els = getEls(elsOrSelector, target);
+	_els.forEach(el => {
+		el.classList.remove(...castToArray(_class));
+	});
+}
